8b653e79cbf29fee1166ea2d746d4d81664bca31
devmeira <gabriel.b.meira@aluno.senai.br>
2026-05-04 22:11:20 -0300

    Aula 05
    O que fizemos na Aula 05

--- Aulas Presenciais/Aula05.py
+++ Aulas Presenciais/Aula05.py
@@ -1,3 +1,37 @@
 # IF e ELSE
 # Hoje tivemos uma apresentação (Slides) - Utilizou o Notebook LM
 
+# Está utilizando o IF e ELSE para falar de Booleanos
+# print(10 < 5) - false
+# print(10 > 5) - true
+# print(10 >= 10) - true
+# print(10 <= 5) - false
+# print(10 != 5) - true
+# print(10 != 10) - false
+# print(10 == 10) - true
+
+# idade = int(input("Digete sua idade: "))
+
+# cnh = input("Tem CNH? (sim/não): ")
+
+# if idade >= 18 and cnh == "sim":
+#     print("Você pode dirigir")
+# else:
+#     print("Você não pode dirigir")
+
+# estudante = True
+# idoso = int(input("Digite idade: "))
+
+# if estudante == True or idoso >= 65:18
+#     print("Ganhou desconto")
+# else:
+#     print("Não foi dessa vez!")
+
+idade = int(input("Digite sua idade: "))
+condicaoFisca = input("Estado de saúde? (Boa/Ruim) ")
+atestadoMedico = input("Tem atestado médico? (Sim/Não) ")
+
+if idade >= 18 and (condicaoFisca == "Boa" or atestadoMedico == "Não"):
+    print("Pode viajar")
+else:
+    print("Não pode viajar")
