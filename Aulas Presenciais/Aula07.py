b3ac4f6de56cc1fa142be1b4a7a3ce760835eb47
devmeira <gabriel.b.meira@aluno.senai.br>
2026-05-18 20:11:59 -0300

    Aula 07 - Antes da Meranda!
    Exercício de while (enquanto) com valor True (verdadeiro). Exemplo de Login e Senha!

---
+++ Aulas Presenciais/Aula07.py
@@ -0,0 +1,41 @@
+### Aula 07 ###
+
+# LAÇOS LOOPS
+
+# WHILE & FOR
+
+# procurar = input("Pesquisar peça: ")
+# estoque = ["Prego", "Porca", "Arruela", "Parafuso", "Mola"]
+
+# for item in estoque:
+#     if item == procurar:
+#         print("Item encontrado no estoque!")
+#         break # Interrompe o laço imediatamente
+# else:
+#     print("Item não encontrado após varredura completa.")
+
+### WHILE
+
+# nome = input("Digite o seu nome: ")
+
+# while nome == "":
+#     print("Você não digitou o seu nome")
+#     nome = input("Por favor, digite o seu nome: ")
+
+# print(f"Olá, {nome}! Tudo bem?")
+
+### DESAFIO
+
+# login = "meira"
+# senha = "123"
+
+# while True:
+
+#     loginUsuario = input("Login: ")
+#     senhaUsuario = input("Senha: ")
+
+#     if login == loginUsuario and senha == senhaUsuario:
+#         print("Você acessou o sistema")
+#         break
+#     else:
+#         print("Login ou Senha incorretos, tenta novamente \n")
